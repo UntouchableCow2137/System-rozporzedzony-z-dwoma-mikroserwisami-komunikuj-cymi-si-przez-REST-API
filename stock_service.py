509b3d88c468cdfd251b0eb0a8273c38a76d5d4b
UntouchableCow2137 <maciej.wydrowski@wp.pl>
2025-11-29 16:36:31 +0100

    stock_service.py

--- stock_service.py
+++ stock_service.py
@@ -3,32 +3,33 @@ import requests
 
 app = Flask(__name__)
 
-# Dane stanów magazynowych przechowywane "na sztywno"
+
 stocks = {
     1: {"productId": 1, "quantity": 15},
     2: {"productId": 2, "quantity": 0},
     3: {"productId": 3, "quantity": 5}
 }
 
-# Adres Serwisu Produktów
+
 PRODUCT_SERVICE_URL = 'http://localhost:8001/products/'
 
 @app.route('/stock/<int:product_id>', methods=['GET'])
 def get_stock(product_id):
-    # Sprawdzamy, czy produkt istnieje w Serwisie Produktów
+   
     product_response = requests.get(PRODUCT_SERVICE_URL + str(product_id))
     if product_response.status_code == 404:
         abort(404, description="Product not found in product service")
     elif product_response.status_code != 200:
         abort(500, description="Error contacting product service")
 
-    # Pobieramy stan magazynowy
+
     stock = stocks.get(product_id)
     if stock is None:
-        # Jeżeli brak danych o stanie, zwracamy quantity=0
+  
         return jsonify({"productId": product_id, "quantity": 0})
     return jsonify(stock)
 
 if __name__ == '__main__':
-    # Uruchamiamy serwer na porcie 8002
+    
     app.run(port=8002)
+
